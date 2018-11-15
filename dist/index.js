322df1354ddac63b8ca3faed10321044da287bb0
Jakob Aschauer <jakob@familie-aschauer.eu>
2018-11-15 13:30:36 +0100

    2 version

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,68 @@
 var form;
 $(document).ready(function () {
-    form = new Form($('#firstname'), $('#lastName'), $('#firstNameMandatory'), $('#lastNameMandatory'), $('#email'), $('#emailMandatory'), $('#mediaChannelSelect'), $('#otherMediaChannel'), $('#newsletter'));
-    hideAll(new Array(/*$('#firstNameMandatory'), $('#lastNameMandatory'),*/ form.emailMandatory, form.otherMediaChannel));
+    form = new Form($('#firstName'), $('#lastName'), $('#firstNameMandatory'), $('#lastNameMandatory'), $('#email'), $('#emailMandatory'), $('#mediaChannelSelect'), $('#otherMediaChannel'), $('#newsletter'), $('.btn-primary'));
+    hideAll(new Array(/*$('#firstNameMandatory'), $('#lastNameMandatory'),*/ form.emailMandatory, form.otherMediaChannel, form.submit));
     form.newsletter.click(newsletterValidation);
     form.email.focusout(newsletterValidation);
-    form.firstName.focusout(firstNameValidation);
     form.lastName.focusout(lastNameValidation);
     form.mediaChannelSelect.change(otherMediaChannelValidation);
+    form.firstName.focusout(firstNameValidation);
 });
 function newsletterValidation() {
     if (form.newsletter.is(':checked') && !form.email.val()) {
         form.emailMandatory.show();
+        form.validationErrors.email = false;
+        submitValidation();
     }
     else {
         form.emailMandatory.hide();
+        form.validationErrors.email = true;
+        submitValidation();
     }
 }
 function firstNameValidation() {
     if (!form.firstName.val()) {
         form.firstNameMandatory.show();
+        form.validationErrors.firstname = false;
+        submitValidation();
     }
     else {
         form.firstNameMandatory.hide();
+        form.validationErrors.firstname = true;
+        console.log(form.validationErrors.getValidationState());
+        submitValidation();
+    }
+}
+function submitValidation() {
+    if (form.validationErrors.getValidationState() == true) {
+        form.submit.show();
+    }
+    else {
+        form.submit.hide();
     }
 }
 function lastNameValidation() {
     if (!form.lastName.val()) {
         form.lastNameMandatory.show();
+        form.validationErrors.lastName = false;
+        submitValidation();
     }
     else {
         form.lastNameMandatory.hide();
+        form.validationErrors.lastname = true;
+        submitValidation();
     }
 }
 function otherMediaChannelValidation() {
     //console.log(form.mediaChannelSelect.val())
     if (form.mediaChannelSelect.val() == 'Other') {
         form.otherMediaChannel.show();
+        //form.validationErrors.otherMediaChannel = true;
+        //submitValidation();
     }
     else {
         form.otherMediaChannel.hide();
+        //form.validationErrors.otherMediaChannel = false;
     }
 }
 function hideAll(objects) {
@@ -47,7 +71,7 @@ function hideAll(objects) {
     }
 }
 var Form = /** @class */ (function () {
-    function Form(firstName, lastName, firstNameMandatory, lastNameMandatory, email, emailMandatory, mediaChannelSelect, otherMediaChannel, newsletter) {
+    function Form(firstName, lastName, firstNameMandatory, lastNameMandatory, email, emailMandatory, mediaChannelSelect, otherMediaChannel, newsletter, submit) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.firstNameMandatory = firstNameMandatory;
@@ -57,6 +81,21 @@ var Form = /** @class */ (function () {
         this.mediaChannelSelect = mediaChannelSelect;
         this.otherMediaChannel = otherMediaChannel;
         this.newsletter = newsletter;
+        this.submit = submit;
+        this.validationErrors = new ValidationErrors();
     }
     return Form;
+}());
+var ValidationErrors = /** @class */ (function () {
+    //otherMediaChannel: boolean;
+    function ValidationErrors() {
+        this.firstname = false;
+        this.lastname = false;
+        this.email = true;
+        //this.otherMediaChannel = false;
+    }
+    ValidationErrors.prototype.getValidationState = function () {
+        return (this.firstname == true && this.lastname == true && this.email == true /*&& this.otherMediaChannel == true*/);
+    };
+    return ValidationErrors;
 }());
--- index.ts
+++ index.ts
@@ -1,38 +1,59 @@
 let form;
 
 $(document).ready(function(){
-    form = new Form($('#firstname'), $('#lastName'), $('#firstNameMandatory'), $('#lastNameMandatory'), $('#email'), $('#emailMandatory'), $('#mediaChannelSelect'), $('#otherMediaChannel'), $('#newsletter'));
+    form = new Form($('#firstName'), $('#lastName'), $('#firstNameMandatory'), $('#lastNameMandatory'), $('#email'), $('#emailMandatory'), $('#mediaChannelSelect'), $('#otherMediaChannel'), $('#newsletter'), $('.btn-primary'));
 
-    hideAll(new Array(/*$('#firstNameMandatory'), $('#lastNameMandatory'),*/ form.emailMandatory, form.otherMediaChannel));
+    hideAll(new Array(/*$('#firstNameMandatory'), $('#lastNameMandatory'),*/ form.emailMandatory, form.otherMediaChannel, form.submit));
 
     form.newsletter.click(newsletterValidation);
     form.email.focusout(newsletterValidation);
-    form.firstName.focusout(firstNameValidation);
     form.lastName.focusout(lastNameValidation);
     form.mediaChannelSelect.change(otherMediaChannelValidation);
+    form.firstName.focusout(firstNameValidation);
 });
 
 function newsletterValidation() {
     if(form.newsletter.is(':checked') && !form.email.val()) {
         form.emailMandatory.show();
+        form.validationErrors.email = false;
+        submitValidation()
     } else {
-        form.emailMandatory.hide();
+        form.emailMandatory.hide();   
+        form.validationErrors.email = true;
+        submitValidation();
     }
 }
 
 function firstNameValidation() {
     if(!form.firstName.val()) {
         form.firstNameMandatory.show();
+        form.validationErrors.firstname = false;
+        submitValidation()
     } else {
         form.firstNameMandatory.hide();
+        form.validationErrors.firstname = true;
+        console.log(form.validationErrors.getValidationState())
+        submitValidation();
+    }
+}
+
+function submitValidation() {
+    if(form.validationErrors.getValidationState() == true) {
+        form.submit.show();
+    } else {
+        form.submit.hide();
     }
 }
 
 function lastNameValidation() {
     if(!form.lastName.val()) {
         form.lastNameMandatory.show();
+        form.validationErrors.lastName = false;
+        submitValidation()
     } else {
         form.lastNameMandatory.hide();
+        form.validationErrors.lastname = true;
+        submitValidation();
     }
 }
 
@@ -40,8 +61,11 @@ function otherMediaChannelValidation() {
     //console.log(form.mediaChannelSelect.val())
     if(form.mediaChannelSelect.val() == 'Other') {
         form.otherMediaChannel.show();
+        //form.validationErrors.otherMediaChannel = true;
+        //submitValidation();
     } else {
         form.otherMediaChannel.hide();
+        //form.validationErrors.otherMediaChannel = false;
     }
 }
 
@@ -61,9 +85,10 @@ class Form {
     mediaChannelSelect: JQuery<HTMLElement>;
     otherMediaChannel: JQuery<HTMLElement>;
     newsletter: JQuery<HTMLElement>;
-    errorCount: number;
+    submit: JQuery<HTMLElement>;
+    validationErrors: ValidationErrors;
 
-    constructor(firstName: JQuery<HTMLElement>, lastName: JQuery<HTMLElement>, firstNameMandatory: JQuery<HTMLElement>, lastNameMandatory: JQuery<HTMLElement>, email: JQuery<HTMLElement>, emailMandatory: JQuery<HTMLElement>, mediaChannelSelect: JQuery<HTMLElement>, otherMediaChannel: JQuery<HTMLElement>, newsletter: JQuery<HTMLElement>) {
+    constructor(firstName: JQuery<HTMLElement>, lastName: JQuery<HTMLElement>, firstNameMandatory: JQuery<HTMLElement>, lastNameMandatory: JQuery<HTMLElement>, email: JQuery<HTMLElement>, emailMandatory: JQuery<HTMLElement>, mediaChannelSelect: JQuery<HTMLElement>, otherMediaChannel: JQuery<HTMLElement>, newsletter: JQuery<HTMLElement>, submit: JQuery<HTMLElement>) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.firstNameMandatory = firstNameMandatory;
@@ -73,6 +98,25 @@ class Form {
         this.mediaChannelSelect = mediaChannelSelect;
         this.otherMediaChannel = otherMediaChannel;
         this.newsletter = newsletter;
-        this.errorCount = 0;
+        this.submit = submit;
+        this.validationErrors = new ValidationErrors();
+    }
+}
+
+class ValidationErrors {
+    firstname: boolean;
+    lastname: boolean;
+    email: boolean;
+    //otherMediaChannel: boolean;
+
+    constructor () {
+        this.firstname = false;
+        this.lastname = false;
+        this.email = true;
+        //this.otherMediaChannel = false;
+    }
+
+    public getValidationState(){
+        return (this.firstname == true && this.lastname == true && this.email == true /*&& this.otherMediaChannel == true*/);
     }
 }
